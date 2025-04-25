a3d95e65cc7fac959bd616fe779d2026ff1c186c
saptarshi20022 <saptarshibose2002@gmail.com>
2025-04-25 23:31:30 +0530

    confidence adjsutments

--- utils/hand_tracker.py
+++ utils/hand_tracker.py
@@ -4,7 +4,7 @@ import mediapipe as mp
 
 class HandDetector:
     def __init__(self, static_image_mode=False, max_num_hands=1, 
-                 min_detection_confidence=0.7, min_tracking_confidence=0.7):
+                 min_detection_confidence=0.5, min_tracking_confidence=0.5):
         """
         Updated for MediaPipe 0.10.3+ with current parameter names
         """
